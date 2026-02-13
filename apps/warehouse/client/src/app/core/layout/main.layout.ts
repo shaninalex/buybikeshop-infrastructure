e7f94d23f98ebad96b83eb06de1c65be5d4f3550
shaninalex <shanin.x@gmail.com>
2026-02-13 09:24:39 +0200

    warehouse: set base layout, components and styling

---
+++ apps/warehouse/client/src/app/core/layout/main.layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './header.component';
+import { SidebarComponent } from './sidebar.component';
+
+@Component({
+    selector: 'main-layout',
+    imports: [HeaderComponent, SidebarComponent],
+    templateUrl: './main.layout.html',
+    styleUrl: './main.layout.css',
+})
+export class MainLayout {}
--- apps/warehouse/client/src/app/pages/app.routes.ts
+++ apps/warehouse/client/src/app/pages/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './home-page/home-page';
+import { PageContainer } from './page.container';
 
 export const routes: Routes = [
     {
         path: '',
-        component: HomePage,
+        component: PageContainer,
+        children: [
+            {
+                path: '',
+                component: HomePage,
+            },
+        ],
     },
 ];
