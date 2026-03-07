e6eb4d912abe66a75f9e50cd2d69c779be15e933
shaninalex <shanin.x@gmail.com>
2026-03-07 14:28:29 +0200

    refactor(schema): better inventory schema

--- resources/database/migrations/20260307101656_partners.up.sql
+++ resources/database/migrations/20260307101656_partners.up.sql
@@ -1,25 +1,33 @@
-CREATE TABLE partners.supplier_roles
+CREATE TABLE partners.partner_roles
 (
     id   SERIAL PRIMARY KEY,
     role varchar UNIQUE
 );
 
+CREATE TABLE partners.partner
+(
+    id         SERIAL PRIMARY KEY,
+    role_id    bigint  NOT NULL,
+    title      varchar not null,
+    created_at timestamp DEFAULT now(),
+
+    FOREIGN KEY (role_id) REFERENCES partners.partner_roles (id)
+);
+
+CREATE TABLE partners.partner_contacts
+(
+    id         SERIAL PRIMARY KEY,
+    contacts   text   NOT NULL,
+    partner_id bigint NOT NULL,
+    created_at timestamp DEFAULT now(),
+
+    FOREIGN KEY (partner_id) REFERENCES partners.partner (id)
+);
+
 CREATE TABLE partners.suppliers
 (
     id         SERIAL PRIMARY KEY,
-    title      varchar UNIQUE NOT NULL,
-    role_id    bigint         NOT NULL,
     created_at timestamp DEFAULT now(),
-
-    FOREIGN KEY (role_id) REFERENCES partners.supplier_roles (id)
-);
-
-CREATE TABLE partners.suppliers_contacts
-(
-    id          SERIAL PRIMARY KEY,
-    contacts    text   NOT NULL,
-    supplier_id bigint NOT NULL,
-    created_at  timestamp DEFAULT now(),
-
-    FOREIGN KEY (supplier_id) REFERENCES partners.suppliers (id)
-);
+    partner_id bigint NOT NULL,
+    FOREIGN KEY (partner_id) REFERENCES partners.partner (id)
+)
--- resources/database/migrations/20260307102329_inventory.up.sql
+++ resources/database/migrations/20260307102329_inventory.up.sql
@@ -1,49 +1,64 @@
 CREATE TABLE inventory.warehouses
 (
-    id        SERIAL PRIMARY KEY,
-    available bigint NOT NULL,
-    address   text   NOT NULL
+    id         SERIAL PRIMARY KEY,
+    name       varchar   NOT NULL,
+    address    text      NOT NULL,
+    code       varchar UNIQUE,
+    created_at timestamp NOT NULL DEFAULT now()
 );
 
-CREATE TABLE inventory.inventory
+CREATE TABLE inventory.inventory_items
 (
-    id        SERIAL PRIMARY KEY,
-    available bigint NOT NULL
+    id  SERIAL PRIMARY KEY,
+    sku varchar UNIQUE
 );
 
 ALTER TABLE catalog.product_variants
-    ADD COLUMN inventory_id bigint;
+    ADD COLUMN inventory_item_id bigint UNIQUE; -- should be not null
 
 ALTER TABLE catalog.product_variants
-    ADD FOREIGN KEY (inventory_id)
-        REFERENCES inventory.inventory (id)
+    ADD FOREIGN KEY (inventory_item_id)
+        REFERENCES inventory.inventory_items (id)
         ON DELETE CASCADE;
 
+CREATE TABLE inventory.inventory_levels
+(
+    item_id      int    NOT NULL,
+    warehouse_id int    NOT NULL,
+
+    -- Physically in warehouse, ready to sell
+    available    bigint NOT NULL DEFAULT 0,
+
+    -- In transit / in delivery, not physically in warehouse yet
+    incoming     bigint NOT NULL DEFAULT 0,
+
+    -- Temporarily held (inspection, QA, allocated for a pick, or internal holds)
+    reserved     bigint NOT NULL DEFAULT 0,
+
+    -- Allocated to customer orders/carts but not shipped yet
+    commited     bigint NOT NULL DEFAULT 0,
+
+    PRIMARY KEY (item_id, warehouse_id),
+    FOREIGN KEY (item_id) REFERENCES inventory.inventory_items (id),
+    FOREIGN KEY (warehouse_id) REFERENCES inventory.warehouses (id)
+);
+
+
+--- DELIVERIES
 
 CREATE TABLE inventory.deliveries
 (
-    id            SERIAL PRIMARY KEY,
-    supplier_id   bigint NOT NULL,
-    warehouse_id  bigint NOT NULL,
-    delivery_date timestamp,
-    status        varchar,
-    created_at    timestamp DEFAULT now(),
-
-    FOREIGN KEY (supplier_id)
-        REFERENCES partners.suppliers (id),
-
-    FOREIGN KEY (warehouse_id)
-        REFERENCES inventory.warehouses (id)
+    id           SERIAL PRIMARY KEY,
+    warehouse_id BIGINT    NOT NULL REFERENCES inventory.warehouses (id),
+    supplier_id  BIGINT    NOT NULL,
+    created_at   TIMESTAMP NOT NULL DEFAULT now()
 );
 
 CREATE TABLE inventory.delivery_items
 (
-    id          SERIAL PRIMARY KEY,
-    delivery_id bigint  NOT NULL,
-    variant_id  bigint  NOT NULL,
-    quantity    integer NOT NULL,
-    -- cost_price numeric,
+    delivery_id BIGINT REFERENCES inventory.deliveries (id),
+    variant_id  BIGINT REFERENCES catalog.product_variants (id),
+    quantity    BIGINT NOT NULL,
 
-    FOREIGN KEY (delivery_id) REFERENCES inventory.deliveries (id),
-    FOREIGN KEY (variant_id) REFERENCES catalog.product_variants (id)
+    PRIMARY KEY (delivery_id, variant_id)
 );
