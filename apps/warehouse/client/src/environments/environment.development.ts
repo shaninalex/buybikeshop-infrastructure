0f8aa0c09f272450d93e4256638d507235b6d7de
shaninalex <shanin.x@gmail.com>
2026-03-23 13:46:26 +0200

    fix(kratos): do not hardcode return urls in login flow config

--- apps/warehouse/client/src/environments/environment.development.ts
+++ apps/warehouse/client/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    LOGIN_PAGE: "http://127.0.0.1:4455/login",
+    LOGIN_PAGE: "http://127.0.0.1:4455/login?return_to=http://127.0.0.1:4200",
     AUTH_SERVER: "http://127.0.0.1:4433"
 };
