cd6cd9f42d5d041cc7e5077ad1ddb8a6aa9386ea
shaninalex <shanin.x@gmail.com>
2026-03-23 16:56:23 +0200

    feat(admin): basic setup + assets

--- apps/admin/admin/web/app.py
+++ apps/admin/admin/web/app.py
@@ -1,13 +1,7 @@
 from starlette.applications import Starlette
 from starlette.types import ASGIApp
 
-from market.web.di import bootstrap
-from market.web.routes import routes
-
-bootstrap()
-
 def create_app() -> ASGIApp:
     return Starlette(
         debug=True,
-        routes=routes,
     )
